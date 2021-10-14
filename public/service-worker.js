ed9ddd2a96bbe78de84d26b106b646a902d4e34d
Kinley Davis <d8tascientist@gmail.com>
2021-10-14 10:22:55 -0400

    update sw with fetch and activate

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,10 +1,12 @@
 const FILES_TO_CACHE = [
     '/',
     "/index.js",
-    "/manifest.json",
+    "/index.html",
+    "/manifest.webmanifest",
     'styles.css',
     "/icons/icon-192x192.png",
-    "/icons/icon-512x512.png" 
+    "/icons/icon-512x512.png",
+    "/db.js"
   ];
   
   const PRECACHE = 'my-site-cache-v1';
@@ -14,47 +16,53 @@ const FILES_TO_CACHE = [
     event.waitUntil(
       caches
         .open(PRECACHE)
-        .then((cache) => cache.addAll(FILES_TO_CACHE))
-        .then(self.skipWaiting())
-    );
-  });
-  
-  // The activate handler takes care of cleaning up old caches.
-//   self.addEventListener('fetch', (event) => {
-//     event.waitUntil(
-//       caches
-//         .keys()
-//         .then((cacheNames) => {
-//           return cacheNames.filter((cacheName) => !currentCaches.includes(cacheName));
-//         })
-//         .then((cachesToDelete) => {
-//           return Promise.all(
-//             cachesToDelete.map((cacheToDelete) => {
-//               return caches.delete(cacheToDelete);
-//             })
-//           );
-//         })
-//         .then(() => self.clients.claim())
-//     );
-//   });
-  
-  self.addEventListener('fetch', (event) => {
-    if (event.request.url.icludes("/api/")) {
-      event.respondWith(
-        caches.open(DATA_CACHE_NAME).then((cachedResponse) => {
-          if (cachedResponse) {
-            return cachedResponse;
-          }
-  
-          return caches.open(RUNTIME).then((cache) => {
-            return fetch(event.request).then((response) => {
-              return cache.put(event.request, response.clone()).then(() => {
-                return response;
-              });
-            });
-          });
+        .then(cache => {
+            var cacheAll = cache.addAll(FILES_TO_CACHE);
+            return cacheAll;
         })
-      );
-    }
-  });
+    );
+    self.skipWaiting();
+    });
   
+self.addEventListener("fetch", function (event) {
+    const { url } = event.request;
+    if (url.includes("/all") || url.includes("/find")) {
+        event.respondWith(
+            caches.open(DATA_CACHE_NAME).then(cache => {
+                return fetch(event.request)
+                    .then(response => {
+                        if (response.status === 200) {
+                            cache.put(event.request, response.clone());
+                        }
+                        return response;
+                    })
+                    .catch(err => {
+                        return cache.match(event.request);
+                    });
+            }).catch(err)
+        );
+    } else {
+        event.respondWith(
+            caches.open(CACHE_NAME).then(cache => {
+                return cache.match(event.request).then(response => {
+                    return response || fetch(event.request);
+                });
+            })
+        );
+    }
+});
+
+self.addEventListener("activate", function (event) {
+    event.waitUntil(
+        caches.keys().then(keyList => {
+            return Promise.all(
+                keyList.map(key => {
+                    if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+                        return caches.delete(key);
+                    }
+                })
+            );
+        })
+    );
+    self.clients.claim();
+});
